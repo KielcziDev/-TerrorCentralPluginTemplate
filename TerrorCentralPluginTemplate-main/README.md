eac727763bc8dcd25293343a6d5ce30bf94f630c
Kielich <69254493+KielcziDev@users.noreply.github.com>
2022-06-17 17:09:23 +0200

    Add files via upload

---
+++ TerrorCentralPluginTemplate-main/README.md
@@ -0,0 +1,8 @@
+# TerrorCentral-TEMPLATE
+Create your own plugins for Terror
+
+Here you can basically create your own plugin that will work in Terror!
+
+You can code basically anything here, just don't change the class name 
+
+Btw, anything in Main will only be executed, so when you create a new method, make sure you call it right there!
---
+++ TerrorCentralPluginTemplate-main/TerrorCentralPluginTemplate/TerrorPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TerrorCentralPluginTemplate
+{
+
+    //Here you can basically create your own plugin that will work for Terrorka!
+    // You can code basically anything here, just don't change
+    //Btw, anything in Main will only be executed, so when you create a new method, make sure you call it right there!
+    //-Kielczi
+
+    public class TerrorPlugin
+    {
+        public static void Main()
+        {
+            MessageBox.Show("Pozdrów Mamusie!");
+        }
+    }
+}
